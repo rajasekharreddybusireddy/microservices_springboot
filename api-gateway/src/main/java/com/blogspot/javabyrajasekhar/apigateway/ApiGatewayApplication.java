ebece5cd9e451b20bad3c6319b6048ca62dc1553
rajasekharreddybusireddy <rajasekharreddy.busi@gmail.com>
2020-02-26 10:04:29 +0530

    first coomit

---
+++ api-gateway/src/main/java/com/blogspot/javabyrajasekhar/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,22 @@
+package com.blogspot.javabyrajasekhar.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public MyFilter myFilter() {
+		return new MyFilter();
+	}
+}
---
+++ eureka-server/src/test/java/com/blogspot/javabyrajasekhar/eurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogspot.javabyrajasekhar.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
